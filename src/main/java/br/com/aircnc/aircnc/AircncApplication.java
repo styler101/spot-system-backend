7eafea87ea941db8d44976b7cbfb017d873680a9
Lucas <lucas.santos@mundowap.com.br>
2023-04-16 10:45:28 -0300

    feat: initial commit

---
+++ src/main/java/br/com/aircnc/aircnc/AircncApplication.java
@@ -0,0 +1,13 @@
+package br.com.aircnc.aircnc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AircncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AircncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/aircnc/aircnc/AircncApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.aircnc.aircnc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AircncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
